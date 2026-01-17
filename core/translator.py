3c9e96ccf6ac8b2a13a9092f326ae0f9fbacf5ee
RonGaRok <Aaronsdsm@gmail.com>
2026-01-17 19:26:31 +0100

    Clean libs branch

--- core/translator.py
+++
@@ -1,9 +0,0 @@
-from deep_translator import GoogleTranslator
-
-def translate(text: str, target_lang: str):
-    if not text:
-        return text
-    try:
-        return GoogleTranslator(source='auto', target=target_lang).translate(text)
-    except Exception:
-        return text
